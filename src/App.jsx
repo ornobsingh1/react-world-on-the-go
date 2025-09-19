216a941ac8092912e4ab11f80c038b455dd9e540
Ornob Singh <ornobsingh14@gmail.com>
2025-09-19 11:05:53 +0600

    adding first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Countries from "./components/Countries/Countries";
+
+function App() {
+  return (
+    <>
+      <h1>React World On The Go...</h1>
+      <Countries />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Countries = () => {
+  return (
+    <div>
+      <h2>In The Countries</h2>
+    </div>
+  )
+}
+
+export default Countries
